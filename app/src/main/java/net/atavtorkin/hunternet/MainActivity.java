7393d8269e6a4c5365e48e273bc1b2549f104f78
av.tavtorkin <av.tavtorkin@gmail.com>
2018-04-10 15:31:49 +0300

    added user authorization

--- app/src/main/java/net/atavtorkin/hunternet/MainActivity.java
+++ app/src/main/java/net/atavtorkin/hunternet/MainActivity.java
@@ -1,13 +1,53 @@
 package net.atavtorkin.hunternet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+              //  new AuthUI.IdpConfig.EmailBuilder().build()
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (requestCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (user != null) {
+                    Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        }
     }
 }
